842f2433665277a4930ef456e4435cd02c8a81fd
Usaid <46266811+usaidpeerzada@users.noreply.github.com>
2024-05-10 01:25:14 +0530

    build folder rm

--- build/utils/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EMAIL_SUBJECT = exports.RESEND = void 0;
-exports.RESEND = "resend";
-exports.EMAIL_SUBJECT = "Open Todo | Please verify your email";
