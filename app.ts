e4d5cef7d84bf572987164dae9836871fdf9dd6b
Usaid <46266811+usaidpeerzada@users.noreply.github.com>
2024-05-10 01:06:32 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -2,17 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { Resend } from "resend";
-
 import { CreateMongoAuthController } from "auth-ez";
 
 import User from "./models/user.model";
 import { Config } from "./utils/types";
 import { EMAIL_SUBJECT, RESEND } from "./utils/constants";
 
+import routes from "./routes/routes";
+
 dotenv.config();
 
 const app = express();
-// const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 const port = process.env.PORT;
 const dbUrl = process.env.OPEN_TODO_DB_URL;
 
@@ -21,12 +22,12 @@ mongoose.connect(dbUrl);
 const authEzConfig: Config = {
   User,
   enableLogs: true,
-  //   emailOptions: {
-  //     enableEmail: true,
-  //     emailType: RESEND,
-  //     emailSdk: resend,
-  //     verificationMailSubject: EMAIL_SUBJECT,
-  //   },
+  emailOptions: {
+    enableEmail: true,
+    emailType: RESEND,
+    emailSdk: resend,
+    verificationMailSubject: EMAIL_SUBJECT,
+  },
 };
 
 const authController = new CreateMongoAuthController(authEzConfig);
@@ -36,5 +37,9 @@ app.use("/api/v1", authController.getRouter());
 app.get("/", (_, res) =>
   res.json({ status: 200, message: "open-todo service" })
 );
+
+app.use("/api/v1", authController.getRouter());
+app.use(express.json());
+app.use("/api/v1", routes);
 
 app.listen(port, () => console.log(`Server is running on ${port}`));
