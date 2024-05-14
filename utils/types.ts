c94b65bd751c2f0dd04c07925f88837c96be4149
Usaid <46266811+usaidpeerzada@users.noreply.github.com>
2024-05-14 22:22:36 +0530

    Update types.ts

--- utils/types.ts
+++ utils/types.ts
@@ -1,42 +1,8 @@
 import { EmailService } from "auth-ez";
 
-export type RouteNames = {
-  loginWithEmailRoute?: string;
-  loginWithUsernameRoute?: string;
-  signupRoute?: string;
-  forgotPasswordRoute?: string;
-  resetPasswordRoute?: string;
-  logoutRoute?: string;
-};
-
-export type EmailOptions = {
-  enableEmail?: boolean;
-  emailType?: string;
-  emailSdk?: any;
-  forgotPasswordSubject: string;
-  forgotPasswordBody: string;
-  verificationMailSubject: string;
-  verificationMailBody: string;
-  emailService: EmailService;
-};
-
 export type Config = {
   User;
   enableLogs?: boolean;
-  hashPassword?;
-  comparePassword?: typeof Function;
-  generateToken?;
-  //   tokenOptions?: {
-  //     expiresIn?: string;
-  //   };
-  routeNames?: {
-    loginWithEmailRoute?: string;
-    loginWithUsernameRoute?: string;
-    signupRoute?: string;
-    forgotPasswordRoute?: string;
-    resetPasswordRoute?: string;
-    logoutRoute?: string;
-  };
   emailOptions?: {
     enableEmail: boolean;
     emailType: string;
